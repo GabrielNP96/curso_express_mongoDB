ed831ecd526dbe953fb979c4ba854432463191ec
GabrielNP96 <gabrielnp1996@gmail.com>
2024-07-12 08:30:03 -0300

    Criando a rota livros

--- server.js
+++ server.js
@@ -1,18 +1,7 @@
-import http from 'http';
+import app from "./src/app.js";
 
 const PORT = 3000;
 
-const rotas = {
-    "/": "curso de Express api",
-    "/livros": "entrei na rota livros.",
-    "/autores": 'entrei na rota autores'
-};
-
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {"Content-Type": "Text/plain"});
-    res.end(rotas[req.url]);
-});
-
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log('servidor escutando');
 });
